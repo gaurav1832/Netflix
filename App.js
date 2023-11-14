fe204f13be52fc3ca847aa00bbbc6da7c7b3fc03
Gaurav Garwa <78600377+gaurav1832@users.noreply.github.com>
2023-11-14 23:26:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { AuthContextProvider } from "./context/authContext";
+
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
